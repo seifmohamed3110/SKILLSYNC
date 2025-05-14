ee4f66e34e84e4d2daaacacefe9afc10834f7bd1
mer486 <merola.g@gmail.com>
2025-05-14 22:33:38 +0300

    Fix: use env variable for Firebase service account

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,16 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebaseServiceAccountKey.json');
+
+const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+if (!serviceAccountBase64) {
+  throw new Error('Missing FIREBASE_SERVICE_ACCOUNT environment variable');
+}
+
+const serviceAccountJSON = Buffer.from(serviceAccountBase64, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(serviceAccountJSON);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
